7aa4c6e27758b698494928ac0d1a894ebcfeae84
Sergei Shulepov <s.pepyakin@gmail.com>
2023-11-22 13:28:01 +0100

    shim: consolidate dock logic in its module

--- sugondat-shim/src/cmd/serve.rs
+++ sugondat-shim/src/cmd/serve.rs
@@ -1,13 +1,6 @@
-use crate::{
-    dock::sovereign::SovereignDock,
-    cli::{serve::Params, DockParams},
-    key::Keypair,
-    sugondat_rpc::Client,
-};
-
-use jsonrpsee::{server::Server, Methods};
-use sugondat_shim_common_sovereign::SovereignRPCServer;
-use tracing::{debug, info};
+use crate::{cli::serve::Params, dock, sugondat_rpc::Client};
+use jsonrpsee::server::Server;
+use tracing::info;
 
 pub async fn run(params: Params) -> anyhow::Result<()> {
     info!(
@@ -15,10 +8,17 @@ pub async fn run(params: Params) -> anyhow::Result<()> {
         params.dock.address, params.dock.port
     );
     let listen_on = (params.dock.address.as_str(), params.dock.port);
-    let maybe_key = crate::cmd::load_key(params.key_management)?;
+    let submit_key = crate::cmd::load_key(params.key_management)?;
     let server = Server::builder().build(listen_on).await?;
     let client = connect_client(&params.rpc.node_url).await?;
-    let handle = server.start(init_docks(client, &params.dock, maybe_key));
+    let methods = dock::init(dock::Config {
+        // TODO: whenever there are more docks, the logic of checking if any at least one is enabled
+        //       and other similar stuff should be in CLI.
+        enable_sovereign: params.dock.enable_sovereign(),
+        client,
+        submit_key,
+    });
+    let handle = server.start(methods);
     handle.stopped().await;
     Ok(())
 }
@@ -26,18 +26,4 @@ pub async fn run(params: Params) -> anyhow::Result<()> {
 async fn connect_client(url: &str) -> anyhow::Result<Client> {
     let client = Client::new(url.to_string()).await?;
     Ok(client)
-}
-
-fn init_docks(
-    client: Client,
-    dock_params: &DockParams,
-    maybe_key: Option<Keypair>,
-) -> Methods {
-    let mut methods = Methods::new();
-    if dock_params.enable_sovereign() {
-        debug!("enabling sovereign adapter dock");
-        let dock = SovereignDock::new(client.clone(), maybe_key);
-        methods.merge(dock.into_rpc()).unwrap();
-    }
-    methods
 }
--- sugondat-shim/src/dock/mod.rs
+++ sugondat-shim/src/dock/mod.rs
@@ -1,4 +1,31 @@
 //! A dock is a component that provides an ad-hoc API consumed by the corresponding adapter in the
 //! rollup.
 
-pub mod sovereign;
+use jsonrpsee::Methods;
+use subxt_signer::sr25519::Keypair;
+
+use crate::sugondat_rpc;
+
+mod sovereign;
+
+/// A configuration for initializing all docks.
+pub struct Config {
+    /// Whether or not to enable the sovereign dock.
+    pub enable_sovereign: bool,
+
+    /// The RPC client handle to the sugondat node.
+    pub client: sugondat_rpc::Client,
+
+    /// The optional key used for signing when submitting blobs.
+    pub submit_key: Option<Keypair>,
+}
+
+/// Initializes all enabled docks and returns the [`Methods`] ready to be registered in the JSON-RPC
+/// server.
+pub fn init(config: Config) -> Methods {
+    let mut methods = Methods::new();
+    if config.enable_sovereign {
+        sovereign::register(&mut methods, &config);
+    }
+    methods
+}

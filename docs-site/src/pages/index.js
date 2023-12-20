7d87615582a585b8657695f6afee2c65dfe31b0f
Robert Habermeier <rphmeier@gmail.com>
2023-12-19 19:02:11 -0600

    some intiial docs pages

--- docs-site/src/pages/index.js
+++ docs-site/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Enter
+            Get Started
           </Link>
         </div>
       </div>

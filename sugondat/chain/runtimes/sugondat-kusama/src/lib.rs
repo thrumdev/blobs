efcf6b6c7c5830e5eaca16b9c44abff3f1e08249
gabriele-0201 <gabriele.miotti2@gmail.com>
2023-12-28 13:10:33 +0100

    extract fee adjustment into new crate

--- sugondat/chain/runtimes/sugondat-kusama/src/lib.rs
+++ sugondat/chain/runtimes/sugondat-kusama/src/lib.rs
@@ -7,12 +7,8 @@
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
 mod constants;
-pub mod fee_adjustment;
 mod weights;
 pub mod xcm_config;
-
-#[cfg(test)]
-mod fee_adjustment_tests;
 
 use cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
 use cumulus_primitives_core::{AggregateMessageOrigin, ParaId};
@@ -21,9 +17,9 @@ use sp_api::impl_runtime_apis;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
     create_runtime_str, generic, impl_opaque_keys,
-    traits::{AccountIdLookup, BlakeTwo256, Block as BlockT},
+    traits::{AccountIdLookup, BlakeTwo256, Block as BlockT, Bounded},
     transaction_validity::{TransactionSource, TransactionValidity},
-    ApplyExtrinsicResult,
+    ApplyExtrinsicResult, FixedPointNumber, Perquintill,
 };
 
 use sp_std::prelude::*;
@@ -53,6 +49,8 @@ use sugondat_primitives::{
     AccountId, AuraId, Balance, BlockNumber, Nonce, Signature, MAXIMUM_BLOCK_LENGTH,
 };
 
+use dynamic_fee_adjustment::{DynamicFeeAdjustment, DynamicLengthToFee};
+use pallet_transaction_payment::Multiplier;
 pub use sp_runtime::{MultiAddress, Perbill, Permill};
 use xcm_config::{KusamaLocation, XcmOriginToTransactDispatchOrigin};
 
@@ -270,12 +268,58 @@ impl pallet_balances::Config for Runtime {
     type MaxFreezes = ConstU32<0>;
 }
 
+parameter_types! {
+    /// Relay Chain `TransactionByteFee` / 10
+    pub const TransactionByteFee: Balance = MILLICENTS;
+
+    // parameters used by BlobsFeeAdjustment
+    // to update NextFeeMultiplier and NextLengthMultiplier
+    //
+    // Common constants used in all runtimes for SlowAdjustingFeeUpdate
+    /// The portion of the `NORMAL_DISPATCH_RATIO` that we adjust the fees with. Blocks filled less
+    /// than this will decrease the weight and more will increase.
+    pub storage TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+
+    /// The adjustment variable of the runtime. Higher values will cause `TargetBlockFullness` to
+    /// change the fees more rapidly.
+    pub AdjustmentVariableBlockFullness: Multiplier = Multiplier::saturating_from_rational(75, 1_000_000);
+    /// that combined with `AdjustmentVariable`, we can recover from the minimum.
+    /// See `multiplier_can_grow_from_zero`.
+    pub MinimumMultiplierBlockFullness: Multiplier = Multiplier::saturating_from_rational(1, 10u128);
+    /// The maximum amount of the multiplier.
+    pub MaximumMultiplierBlockFullness: Multiplier = Bounded::max_value();
+
+    // TODO: is this OK?
+    pub MaximumBlockLength: u32 = MAXIMUM_BLOCK_LENGTH;
+    pub storage TargetBlockSize: Perquintill = Perquintill::from_percent(16); // 0.8MiB
+
+    //  v = p / k * (1 - s*) = 0.3 / (300 * (1 - 0.16))
+    //  at most 30% (=p) fees variation in one hour, 300 blocks (=k)
+    pub AdjustmentVariableBlockSize: Multiplier = Multiplier::saturating_from_rational(1, 840);
+    // TODO: decide the value of MinimumMultiplierBlockSize, https://github.com/thrumdev/blobs/issues/154
+    pub MinimumMultiplierBlockSize: Multiplier = Multiplier::saturating_from_rational(1, 10u128);
+    pub MaximumMultiplierBlockSize: Multiplier = Bounded::max_value();
+}
+
+type BlobsFeeAdjustment<T> = DynamicFeeAdjustment<
+    T,
+    TargetBlockFullness,
+    AdjustmentVariableBlockFullness,
+    MinimumMultiplierBlockFullness,
+    MaximumMultiplierBlockFullness,
+    MaximumBlockLength,
+    TargetBlockSize,
+    AdjustmentVariableBlockSize,
+    MinimumMultiplierBlockSize,
+    MaximumMultiplierBlockSize,
+>;
+
 impl pallet_transaction_payment::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type OnChargeTransaction = pallet_transaction_payment::CurrencyAdapter<Balances, ()>;
     type WeightToFee = WeightToFee;
-    type LengthToFee = fee_adjustment::BlobsLengthToFee<Self>;
-    type FeeMultiplierUpdate = fee_adjustment::BlobsFeeAdjustment<Self>;
+    type LengthToFee = DynamicLengthToFee<Self, Balance, TransactionByteFee>;
+    type FeeMultiplierUpdate = BlobsFeeAdjustment<Self>;
     type OperationalFeeMultiplier = ConstU8<5>;
 }
 

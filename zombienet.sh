1efcf5811d08c19d2c8cc8db5bf273b9e1734b3a
Sergei Shulepov <s.pepyakin@gmail.com>
2024-01-11 15:51:19 +0000

    zombienet: detect required binaries
    Printing suggestions on how to fix those.

--- zombienet.sh
+++ zombienet.sh
@@ -9,4 +9,19 @@ Available at https://github.com/paritytech/zombienet"
   exit 1
 fi
 
+if ! [ -x "$(command -v polkadot)" ]; then
+  echo "\
+'polkadot' is not found in PATH. Install polkadot.
+
+To obtain, refer to https://github.com/paritytech/polkadot-sdk/tree/master/polkadot#polkadot"
+    exit 1
+fi
+
+if ! [ -x "$(command -v sugondat-node)" ]; then
+  echo "\
+'sugondat-node' is not found in PATH. cd to 'sugondat/chain' and run 'cargo build --release'
+and add the result into your PATH."
+  exit 1
+fi
+
 zombienet spawn -p native --dir zombienet testnet.toml

7357955cbca43069309930be0f12bdb09c8e88c5
Robert Habermeier <rphmeier@gmail.com>
2024-01-22 14:23:47 -0600

    bump spec version for kusama runtime

--- sugondat/chain/runtimes/sugondat-kusama/src/lib.rs
+++ sugondat/chain/runtimes/sugondat-kusama/src/lib.rs
@@ -122,7 +122,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("blobchain-kusama"),
     impl_name: create_runtime_str!("blobchain-kusama"),
     authoring_version: 1,
-    spec_version: 1,
+    spec_version: 1001,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,

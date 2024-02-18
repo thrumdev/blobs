995b386ec2bd875ec5d5209501388b7520a366f3
Robert Habermeier <rphmeier@gmail.com>
2024-02-17 19:42:31 -0600

    docs: some stylization improvements

--- docs-site/src/pages/index.js
+++ docs-site/src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Ikura: Data Availability on Polkadot`}
+      description="Data Availability on Polkadot">
       <HomepageHeader />
       <main>
         <HomepageFeatures />

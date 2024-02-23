14e11f4d1fbb833c23b3e5e066610752f092bbdc
Sergei Shulepov <s.pepyakin@gmail.com>
2024-02-23 22:58:10 +0700

    shim/cli: `query block` --json flag
    Which allows outputting block data as JSON.

--- ikura/shim/src/cmd/query/block.rs
+++ ikura/shim/src/cmd/query/block.rs
@@ -2,29 +2,33 @@ use super::{connect_rpc, get_block_at};
 use crate::cli::query::block::Params;
 
 pub async fn run(params: Params) -> anyhow::Result<()> {
-    let Params { rpc, block } = params;
+    let Params { rpc, block, json } = params;
 
     let client = connect_rpc(rpc).await?;
     let block = get_block_at(&client, block).await?;
 
-    println!("Block: #{}", block.number);
-    println!("  Hash: 0x{}", hex::encode(&block.hash[..]));
-    println!("  Parent Hash: 0x{}", hex::encode(&block.parent_hash[..]));
-    println!("  Blobs Root: 0x{}", hex::encode(&block.tree_root.root[..]));
-    println!("  Min Namespace: {}", block.tree_root.min_ns);
-    println!("  Max Namespace: {}", block.tree_root.max_ns);
-    println!("  Timestamp: {}", block.timestamp);
-    println!(
-        "  Blob Count: {} ({} bytes)",
-        block.blobs.len(),
-        block.blobs.iter().map(|b| b.data.len()).sum::<usize>(),
-    );
-    for (i, blob) in block.blobs.into_iter().enumerate() {
-        println!(" Blob #{}", i);
-        println!("    Extrinsic Index: {}", blob.extrinsic_index);
-        println!("    Namespace: {}", &blob.namespace);
-        println!("    Size: {}", blob.data.len());
+    if json {
+        let json = serde_json::to_string_pretty(&block)?;
+        println!("{}", json);
+    } else {
+        println!("Block: #{}", block.number);
+        println!("  Hash: 0x{}", hex::encode(&block.hash[..]));
+        println!("  Parent Hash: 0x{}", hex::encode(&block.parent_hash[..]));
+        println!("  Blobs Root: 0x{}", hex::encode(&block.tree_root.root[..]));
+        println!("  Min Namespace: {}", block.tree_root.min_ns);
+        println!("  Max Namespace: {}", block.tree_root.max_ns);
+        println!("  Timestamp: {}", block.timestamp);
+        println!(
+            "  Blob Count: {} ({} bytes)",
+            block.blobs.len(),
+            block.blobs.iter().map(|b| b.data.len()).sum::<usize>(),
+        );
+        for (i, blob) in block.blobs.into_iter().enumerate() {
+            println!(" Blob #{}", i);
+            println!("    Extrinsic Index: {}", blob.extrinsic_index);
+            println!("    Namespace: {}", &blob.namespace);
+            println!("    Size: {}", blob.data.len());
+        }
     }
-
     Ok(())
 }
